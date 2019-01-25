c331d653b9cdd0699be612e5f82dc77afef7f6fb
Jose <jdvazquezdev@gmail.com>
2019-01-26 00:02:25 +0100

    Functionality finished
    - The html is updated depending on the result.
    - Two counters indicate the balance of victories and defeats.
    -Funcitonality completed.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- // Required constants to modify html
+  // Required constants to modify html
  const imgPlayerChoice = document.getElementById('playerChoice');
  const imgComputerChoice = document.getElementById('computerChoice');
 
@@ -12,28 +12,45 @@
      button => button.addEventListener('click', startGame)
  );
 
+let positiveScore = 0;
+let negativeScore = 0;
+
  function startGame(event) {
      // Determine player's choice
      const button = event.currentTarget;
      const playerChoice = button.dataset.choice;
-     console.log(playerChoice);
+    // console.log(playerChoice);
 
      // Determine computer's choice
      const computerChoice = getComputerChoice();
-     console.log(computerChoice);
+     //console.log(computerChoice);
 
      // Determine who wins
      const winner = getWinner(playerChoice, computerChoice);
-     console.log(`El ganador es ${winner}`);
+     //console.log(`El ganador es ${winner}`);
 
      //Show results
+     imgPlayerChoice.setAttribute('src', `images/${playerChoice}.png` );
+     imgComputerChoice.setAttribute('src', `images/${computerChoice}.png`);
+
+     let result; 
+
      if (winner === 'player') {
-
+        result = 'won';
+        ++positiveScore;
      } else if (winner === 'computer') {
-
-     } else {
-
+        result =  'lost';
+        ++negativeScore;
+     } else { // Draw
+        result = 'draw';
      }
+
+    pResult.innerHTML = `You ${result}
+                        playing <strong>${playerChoice}</strong> 
+                        against <strong>${computerChoice}</strong>.`;
+
+    pScore.innerHTML = `You won ${positiveScore} times. 
+                        You lost ${negativeScore} times.`
 
  }
 
